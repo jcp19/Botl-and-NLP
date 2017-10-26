b00ce34558c4bcb01b4573d4ee5798b3e24b503b
João Pereira <joaopereira.19@gmail.com>
2017-10-26 17:22:58 +0100

    refactored regexs, added tests

--- main.pl
+++ main.pl
@@ -9,14 +9,14 @@ $/ = '';
 
 my $graph = Graph::Undirected->new;
 
-my $PM = qr{\b[A-Z][\w-]*\w};
-my $de = qr{d[aoe]s?};
-my $s = qr{[\n ]};
-my $np = qr{$PM (?: $s $PM | $s $de $s $PM )*}x;
-
+#my $PM = qr{\b[A-Z][\w-]*\w};
+#my $de = qr{d[aoe]s?};
+#my $s = qr{[\n ]};
+#my $np = qr{$PM (?: $s $PM | $s $de $s $PM )*}x;
+#
 my $window_size = 7;
 # my $window = qr{($np) (?: $s+ (?: \w+|($np))**0..$window_size)};
-my $window = qr{($np) (?: $s+ (\w+))*};
+# my $window = qr{($np) (?: $s+ (\w+))*};
 
 #while(<>){
 #  while(/($np) (?=(?: $s+ (\w+))*)/xg){
@@ -27,19 +27,48 @@ my $window = qr{($np) (?: $s+ (\w+))*};
 #  } 
 #}
 
+# Convention: A regex nammed XX_Y captures Y groups. We can ommit Y if it equals 0.
+my $NP = qr/\{\{ [^}]* \}\}/x;
+my $NP_1 = qr/\{\{( [^}]*) \}\}/x;
+my $spaces_and_points = qr/(?: \s|[!.?,;:])*/;
+my $words = qr/(?: \w|\d)+/x;
+my $window = qr/(?: $spaces_and_points (?: $words| $NP)){0..$window_size}/x;
+
+# Tests:
+print "success 1\n" if "{{Joao}}" =~ /$NP/x;
+print "success 2: $1\n" if "{{Joao}}" =~ /$NP_1/x;
+print "success 3\n" if " . ," =~ /$spaces_and_points/x;
+print "success 4\n" if "ola" =~ /$words/x;
+print "success 5\n" if " ola" =~ /$window/x;
+
+
 while(<>){
+#  my $line = $_;
+#  # deteta os nomes do ficheiro anotado
+#  while($line =~ /\{\{( [^}]*)  \}\}  (?=((?:.|\n)*)) /gx){
+#    my $name = $1;
+#    my $tail = $2;
+#    # este devia dar!
+#    # tem de ser assim, no ciclo, senao imprime sempre o ultimo resultado
+#    for my $i (1..$window_size){
+#       $tail =~ /^(?: \s*[!.?,;:]*\s* (?:\w+|\{\{( [^}]*)  \}\})){1,$i}/gx;
+#       print "::: $name :: $tail :: $1 :: $2 \n" 
+#    };
+#
   my $line = $_;
-  # deteta os nomes do ficheiro anotado
-  while($line =~ /\{\{( [^}]*)  \}\}  (?=((?:.|\n)*)) /gx){
-    my $name = $1;
-    my $tail = $2;
-    # este devia dar!
-    my @cenas = $tail =~ /^(?: \s*[!.?,;:]*\s* (?:([\w]+)|\{\{( [^}]*)  \}\})){1,7}/gx;
+  my $matches;
+#  while($line =~ /\{\{( [^}]*)  \}\}  (?=((?:.|\n)*)) /gx){
+  while($matches = $line =~ /$NP_1 ($window)/gx){
+    print ":::: $1 :: $matches :: $2 \n";
+  }
+
+
+
+
+
 #    for my $cena (@cenas){
 #      print $cena; 
 #    }
-    my $shit = @cenas;
-    print "::: $name :: $shit ::$tail :: $2 \n" 
  
     
     #print "::: $1 :: $line :: $2 $3\n";
@@ -54,7 +83,7 @@ while(<>){
 #    my @x = /(?=(?: $s+ (?: [\w-]+ | \{\{.*\}\}) ){1,7})/gx;
 #    my $y = join(", ", @x);
 #    print("$y\n");
-  }
+#  }
 }
 
 # simple example of 'relation' insertion in a graph
