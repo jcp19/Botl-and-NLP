8bfc63063ade99c9020bc51b4db1fa3e627527b5
Manel18 <peaceandhendrix@gmail.com>
2017-11-03 14:24:58 +0000

    minor update

--- precedentes.pl
+++ precedentes.pl
@@ -2,7 +2,6 @@
 use strict;
 use Graph;
 use utf8::all;
-use Util::Any;
 
 # process individual paragraphs
 $/ = ''; 
@@ -16,7 +15,7 @@ my $words = qr/(?: \w|\d)+/x;
 my $window = qr/(?: $spaces_and_points (?: $words| $NP)){0,$window_size}/x;
 #my $wleft = qr/(?: (?: \b\w+\b) $spaces_and_points){0,2}/x;
 my $wleft = qr/(?: (?: \w+) (?: \s|,)*){0,2}/x;
-my @forget =('o','a','os','as','de','do','da','dos','das','para','e','em','sua','ou');
+my @forget =('o','a','os','as','de','do','da','dos','das','para','e','em','sua','ou','que');
 
 # Relations graph
 # my $graph = Graph::Undirected->new(countedged => 1, countvertexed => 1);
@@ -25,42 +24,6 @@ my %nodes;
 my %edges;
 my %names;
 
-# takes a graph prints graphviz representation
-# of it
-sub print_graph {
-  my $graph = shift;
-  print "graph {\n";
-  my @t = split(/,/, $graph->stringify());
-  foreach(@t){
-    my $catches = s/(.*)=(.*)/"$1"--"$2"/g;
-    my $line = $_;
-    if ($catches == 0){
-      s/(.*)/"$1"/;
-      $line = $_;
-    }
-    print "$line\n";
-  }
-  print "}\n";
-}
-
-# Tests:
-sub run_tests_regex {
-  print "success 1\n" if "{{Joao}}" =~ /$NP/x;
-  print "success 2: $1\n" if "{{Joao}}" =~ /$NP_1/x;
-  print "success 3\n" if " . ," =~ /$spaces_and_points/x;
-  print "success 4\n" if "ola" =~ /$words/x;
-  print "success 5\n" if " ola" =~ /$window/x;
-}
-
-# simple example of 'relation' insertion in a graph
-# and show the print function result
-sub run_test_print_graph {
-  $graph = $graph->add_edge("Joao", "Manel");
-  $graph = $graph->add_edge("Rui", "Miguel");
-  $graph = $graph->add_edge("Manel", "Rui");
-  print_graph($graph);
-}
-
 sub print_possible_adjs {
   for my $key (keys %names){
     print "$key:\n";
@@ -68,14 +31,6 @@ sub print_possible_adjs {
       print "\t$madj: $names{$key}{$madj}\n";
 	}
   }
-}
-
-sub stronger_relations {
-    for my $key (keys %edges){
-        my ($name1, $name2) = split ':', $key;
-        $graph = $graph->delete_edge($name1, $name2) if ((($edges{$key} / $nodes{$name1}) < 0.5) && (($edges{$key} / $nodes{$name2}) < 0.5));
-        # print("$key :::: $name1 :: $name2 :: $nodes{$name1} :: $nodes{$name2}\n");
-    }
 }
 
 # Main code:
