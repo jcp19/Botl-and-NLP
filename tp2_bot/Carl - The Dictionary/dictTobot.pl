fbcb3d65f83743d866386ddce741a82cd276f6bf
forlemon <nunocv96@gmail.com>
2018-02-01 04:10:53 +0000

    Correction of Dictionary Bot "To Dos"

--- tp2_bot/Carl - The Dictionary/dictTobot.pl
+++ tp2_bot/Carl - The Dictionary/dictTobot.pl
@@ -6,5 +6,5 @@ use utf8::all;
 #TO DO
 #Processar paragrafo a paragrafo
 #Sempre que encontrar /[A-Z\-]{2,}\n/ -> (word to define)
-#Apanha tudo após /Defn:/ até linha em branco (definition)
-#Resultando na regra RESPOND TO "Definition of (word)" WITH definition 
+#Apanha tudo após /Defn:/ até linha em branco -> (definition)
+#Resultando na regra RESPOND TO "Definition of /word/i" WITH definition 
